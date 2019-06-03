cba8e99e50d26da631f1872c5e56c699b0dd5105
mrmarvel007 <51308681+mrmarvel007@users.noreply.github.com>
2019-06-03 16:00:37 +0530

    Create MHToolkit.sh

---
+++ MHToolkit.sh
@@ -0,0 +1,23 @@
+figlet Marvellous
+figlet hackers
+figlet toolkit
+figlet MR 
+figlet MARVEL
+figlet 007
+apt update && apt upgrade
+apt install unstable-repo git figlet
+figlet installing
+figlet metasploit
+pkg install metasploit
+figlet installing
+figlet kali
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+figlet ./start-kali.sh
+git clone https://github.com/Gameye98/Lazymux
+git clone https://github.com/trustedsec/social-en>
+figlet goodbye
+figlet thankyou
+apt install toilet
+toilet MR
+toilet MARVEL
+toilet 007
